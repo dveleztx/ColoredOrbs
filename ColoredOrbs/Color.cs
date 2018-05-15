9b8eddfa853461567ff806a7b98ce4eb98f4360c
David Velez <dveleztx@gmail.com>
2018-05-15 18:41:36 -0500

    Created Color class as specified in my book challenge

---
+++ ColoredOrbs/Color.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ColoredOrbs
+{
+    internal class Color
+    {
+        // Instance Variables
+        int blue;
+        int green;
+        int red;
+        int alpha;
+
+        // Constructors
+        public Color(int blue, int green, int red)
+        {
+            this.blue = blue;
+            this.green = green;
+            this.red = red;
+            this.alpha = 255;
+        }
+
+        public Color (int blue, int green, int red, int alpha)
+        {
+            this.blue = blue;
+            this.green = green;
+            this.red = red;
+            this.alpha = alpha;
+        }
+
+        // Getters
+        public int GetBlue()
+        {
+            return blue;
+        }
+
+        public int GetGreen()
+        {
+            return green;
+        }
+
+        public int GetRed()
+        {
+            return red;
+        }
+
+        public int GetAlpha()
+        {
+            return alpha;
+        }
+
+        public float GetGrayScale()
+        {
+            return (float)(blue + green + red) / 3;
+        }
+
+        // Setters
+        public void SetBlue (int blue)
+        {
+            this.blue = blue;
+        }
+
+        public void SetGreen (int green)
+        {
+            this.green = green;
+        }
+
+        public void SetRed (int red)
+        {
+            this.red = red;
+        }
+
+        public void SetAlpha (int alpha)
+        {
+            this.alpha = alpha;
+        }
+    }
+}
